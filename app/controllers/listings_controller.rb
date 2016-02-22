d3da216f651a7ad1ea4b220aa9a3c200e7d355c6
Aleksandra Bondzic <bondzica@gmail.com>
2016-02-21 23:09:26 -0600

    Added form and styling

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,6 +2,8 @@ class ListingsController < ApplicationController
 
   def index
     pins = $pinterest.get_pins(query: 'etsy listing').data 
+    @listings = []
+    pins.each { |pin| @listings << Listing.new(pin) }
   end
 
 end
